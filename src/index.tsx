89d3326f7fd94236a78ccc72f246f963d4400688
Eli José Carrasquero <ielijose@gmail.com>
2023-08-12 15:32:31 -0600

    feat: add button type and button themes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
-import React, { useCallback } from 'react';
-import NoRampButton from './components/NoRampButton/NoRampButton';
+import React from 'react';
+import './styles.css';
+
+import { useCallback } from 'react';
+import LoadingIcon from './components/LoadingIcon';
+import NoRampLogo from './components/NoRampLogo';
 import useNoRampModal from './hooks/useNoRampModal';
 import { NoRampConfig } from './types';
+import { TYPE_LABELS } from './constants';
 
 export const NoRampOneClick = ({
   priceId,
   testnet = false,
+  type = 'buy',
+  buttonTheme = 'dark',
   ...config
 }: NoRampConfig) => {
   const { open, isOpen } = useNoRampModal({
@@ -18,5 +25,30 @@ export const NoRampOneClick = ({
     open();
   }, [open]);
 
-  return <NoRampButton onClick={handlePay} loading={isOpen} />;
+  // return <NoRampButton onClick={handlePay} loading={isOpen} />;
+
+  const renderIcon = () => {
+    if (isOpen) {
+      return <LoadingIcon />;
+    }
+
+    return <NoRampLogo />;
+  };
+
+  const isPlain = type === 'plain';
+
+  const label = TYPE_LABELS[type];
+
+  return (
+    <button
+      className={`button button-${buttonTheme}`}
+      type="button"
+      onClick={handlePay}
+      disabled={isOpen}
+    >
+      <span className="icon">{renderIcon()}</span>
+      {!isPlain && <span>{label} with NoRamp</span>}
+      {/* <span className="">Buy with NoRamp</span> */}
+    </button>
+  );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,26 @@
+export type ButtonType =
+  | 'buy'
+  | 'donate'
+  | 'plain'
+  | 'book'
+  | 'checkout'
+  | 'subscribe'
+  | 'add-money'
+  | 'contribute'
+  | 'order'
+  | 'reload'
+  | 'rent'
+  | 'support'
+  | 'tip'
+  | 'top-up'
+  | 'continue';
+
 export type NoRampConfig = {
   priceId: string;
   testnet?: boolean;
   theme?: 'light' | 'dark' | null;
+  buttonTheme?: 'light' | 'dark' | null;
+  type?: ButtonType;
   onEvent?: (payload: any) => void;
   onSuccess?: (payload: any) => void;
   onFailure?: (payload: any) => void;
