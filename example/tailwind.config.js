0c1a61281e359664da2f83f09b9d1e82aa13f918
Eli José Carrasquero <ielijose@gmail.com>
2023-10-23 17:25:56 -0600

    try

--- example/tailwind.config.js
+++ example/tailwind.config.js
@@ -3,8 +3,8 @@ const { nextui } = require('@nextui-org/react');
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+    './example/src/**/*.{js,ts,jsx,tsx}',
+    './example/node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
